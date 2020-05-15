8dbda873f163750c9d5352a689facc6cef005dc3
Mamadou Diop <mamadou.diop@responsetap.com>
2020-05-15 15:56:50 +0200

    fix typo

--- src/components/Simple.js
+++ src/components/Simple.js
@@ -13,9 +13,9 @@ export default function Simple() {
                     </video>
                 </div>
                 <div className="simple-solution-content">
-                    <h3>Simple solutions for complexed connections!</h3>
+                    <h3>Simple solutions for complex connections!</h3>
                     <br/>
-                    <p>Planning an Erasmus meeting, joining friends living abroad, gathering with family.</p><h4> Pading is as simple solution built to bring you closer with your friends and family</h4>
+                    <p>Planning an Erasmus meeting, joining friends living abroad, gathering with family.</p><h4> Pading is a simple solution built to bring you closer with your friends and family</h4>
                 </div>
             </div>
             
