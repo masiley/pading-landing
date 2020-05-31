b6f0fa06197d4171260ee9334799b49c144c7259
louisburette <louis.burette@ieseg.fr>
2020-05-31 13:31:16 +0200

    better responsive design

--- src/components/MyClimate.js
+++ src/components/MyClimate.js
@@ -8,8 +8,9 @@ export default function Simple() {
             <div className="myclimate">
                 <div className="myclimate-content">
                     <h2>We love our planet</h2>
+                    <br/>
                     <p>If like us, you face inner conflict between your passion for travelling and your concerns for ecology, we can do something.</p>
-                    <h3>Pading calculate the climate impact of each destination. You can compensate your Co2 emissions and we plant trees.</h3>
+                    <h4>Pading calculate the climate impact of each destination. You can compensate your Co2 emissions and we plant trees.</h4>
                 </div>
                 <div className="myclimate-img">
                 <i className="fas fa-globe-europe fa-10x"></i>
