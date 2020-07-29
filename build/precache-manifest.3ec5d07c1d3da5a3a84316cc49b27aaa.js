706be00fcf4780df2c63bd3f29131ce62ca43147
Mamadou Diop <mamadou.diop@responsetap.com>
2020-07-29 14:58:39 +0100

    fix typo

---
+++ build/precache-manifest.3ec5d07c1d3da5a3a84316cc49b27aaa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1bc6701f2f91dc3bbaec5e3ed347c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94468a1bc75d14360247",
+    "url": "/static/css/main.e66c31e5.chunk.css"
+  },
+  {
+    "revision": "02fcd29050caa3b9ec96",
+    "url": "/static/js/2.08f7635d.chunk.js"
+  },
+  {
+    "revision": "2bcb765c419343997064775709c0fc81",
+    "url": "/static/js/2.08f7635d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94468a1bc75d14360247",
+    "url": "/static/js/main.6de2f2a2.chunk.js"
+  },
+  {
+    "revision": "56f42f39b3f1f2580d0b",
+    "url": "/static/js/runtime-main.d0bde822.js"
+  },
+  {
+    "revision": "2672a86eafc108b1cbee95cc11813c46",
+    "url": "/static/media/catsbasket.2672a86e.mp4"
+  },
+  {
+    "revision": "114667861f2f3d193e1f43cd32fce8e6",
+    "url": "/static/media/happycats.11466786.mp4"
+  },
+  {
+    "revision": "d265acaa9ec2cd86accd39f04ed0d3c7",
+    "url": "/static/media/padingapp.d265acaa.mp4"
+  }
+]);
--- src/components/MyClimate.js
+++ src/components/MyClimate.js
@@ -10,7 +10,7 @@ export default function Simple() {
                     <h2>We love our planet</h2>
                     <br/>
                     <p>Choosing train, offsetting carbon footprint to make travel more eco-friendly.</p>
-                    <h4>Pading calculate the climate impact of each destination so you can give your preference to low carbon footrip destination and compensate your Co2 emissions.</h4>
+                    <h4>Pading calculates the climate impact of each destination so you can give your preference to low carbon footprint destination and compensate your Co2 emissions.</h4>
                 </div>
                 <div className="myclimate-img">
                 <i className="fas fa-globe-europe fa-10x"></i>
