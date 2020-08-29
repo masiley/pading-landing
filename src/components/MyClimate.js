03551f0299b948e8e6c9e99927ccfc8c6f21276b
louisburette <louis.burette@ieseg.fr>
2020-08-29 11:47:04 +0200

    changes

--- src/components/MyClimate.js
+++ src/components/MyClimate.js
@@ -9,8 +9,8 @@ export default function Simple() {
                 <div className="myclimate-content">
                     <h2>We love our planet</h2>
                     <br/>
-                    <p>Choosing train, offsetting carbon footprint to make travel more eco-friendly.</p>
-                    <h4>Pading calculates the climate impact of each destination so you can give your preference to low carbon footprint destination and compensate your Co2 emissions.</h4>
+                    <p>Offsetting carbon footprint to make travel more eco-friendly</p>
+                    <h4>Pading calculates the climate impact of each destination so you can give your preference to low carbon footprint destination and compensate your Co2 emissions</h4>
                 </div>
                 <div className="myclimate-img">
                 <i className="fas fa-globe-europe fa-10x"></i>
