368d121dfd33bbc8017308c929d66510339ea75a
louisburette <louis.burette@ieseg.fr>
2020-12-16 12:04:38 +0100

    loader landingpage

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ export default class App extends React.Component {
           {setTimeout(() => document.getElementById("big-loader").style.visibility="hidden", 3000)}
 
           { isIOS ? 
-            <div><img src={bigLoaderPadingGif} className="big-loader"/></div>
+            <div><img src={bigLoaderPadingGif} id="big-loader" /></div>
           :
             <video id="big-loader" autoPlay="autoplay" muted playsInline>
               <source src={bigLoaderPadingVideo} alt="loader pading app"/>
